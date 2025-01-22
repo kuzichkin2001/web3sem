d3f64b3875726291bc6f57dc9b0b25f4838194ff
kuzichkin2001 <kuzichkin.p@yandex.ru>
2025-01-23 00:53:24 +0400

    Jopa blyat nooooo nahoooooy

---
+++ DAO/OutboxEntry.cs
@@ -0,0 +1,15 @@
+﻿namespace DAO
+{
+    public class OutboxEntry
+    {
+        public int Id { get; set; }
+
+        public string Destination { get; set; }
+
+        public int MessageBody { get; set; }
+
+        public int MessageContentType { get; set; }
+
+        public OutboxEntryStatus MessageStatus { get; set; }
+    }
+}
---
+++ DAO/OutboxEntryStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace DAO
+{
+    public enum OutboxEntryStatus
+    {
+        Error = 0,
+        InProcess = 1,
+        AlreadySent = 2,
+    }
+}
